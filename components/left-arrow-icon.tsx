f23514f6df46c8abed0d95b62cc6abfa4fd2ba48
Lucas Vilar <lucas.vilar360@gmail.com>
2024-02-29 16:01:03 -0300

    fix: corrected invalid property usage in svg icons

--- components/left-arrow-icon.tsx
+++ components/left-arrow-icon.tsx
@@ -1,7 +1,7 @@
 export function LeftArrowIcon() {
     return (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M14 16L10 12L14 8" stroke="#737380" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M14 16L10 12L14 8" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     )
 }
--- components/right-arrow-icon.tsx
+++ components/right-arrow-icon.tsx
@@ -1,7 +1,7 @@
 export function RightArrowIcon() {
     return (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10 16L14 12L10 8" stroke="#737380" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M10 16L14 12L10 8" stroke="#737380" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     )
 }
